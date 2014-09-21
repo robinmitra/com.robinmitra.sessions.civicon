648e838b38fb5fab7f6c2d323c831c2af5dd3401
Robin Mitra <robinmitra1@gmail.com>
2014-09-21 15:11:07 +0100

    Add DAO, BAO and API for CiviconSession entity

---
+++ api/v3/CiviconSession.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * CiviconSession.create API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+function _civicrm_api3_civicon_session_create_spec(&$spec) {
+  // $spec['some_parameter']['api.required'] = 1;
+}
+
+/**
+ * CiviconSession.create API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_civicon_session_create($params) {
+  return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+/**
+ * CiviconSession.delete API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_civicon_session_delete($params) {
+  return _civicrm_api3_basic_delete(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+/**
+ * CiviconSession.get API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @throws API_Exception
+ */
+function civicrm_api3_civicon_session_get($params) {
+  return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
