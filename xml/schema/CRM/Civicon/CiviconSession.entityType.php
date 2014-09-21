99b2ea545b61be46e12d44d411d779afc381a022
Robin Mitra <robinmitra1@gmail.com>
2014-09-21 15:08:02 +0100

    Add the DB schema for CiviconSession entity

---
+++ xml/schema/CRM/Civicon/CiviconSession.entityType.php
@@ -0,0 +1,11 @@
+<?php
+// This file declares a new entity type. For more details, see "hook_civicrm_entityTypes" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CiviconSession',
+    'class' => 'CRM_Civicon_DAO_CiviconSession',
+    'table' => 'civicrm_civiconsession',
+  ),
+);
